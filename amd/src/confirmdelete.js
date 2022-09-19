82edab367ab0ce446db66cbaf0c369f76e9d7048
Riasat Mahbub <Riasat.Mahbub@brainstation-23.com>
2022-09-19 17:38:45 +0600

    amd/src/confirmdelete: rewrote entire file in es6 syntax

--- amd/src/confirmdelete.js
+++ amd/src/confirmdelete.js
@@ -14,52 +14,46 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- * @package   local_pokemon_tracker
+ *
  * @copyright 2020, Riasat Mahbub <riasat.mahbub@brainstation-23.com>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-define([
-    'jquery',
-    'core/ajax',
-    'core/str',
-    'core/modal_factory',
-    'core/modal_events',
-    'core/notification'
-], function($,
-             Ajax,
-             str,
-             ModalFactory,
-             ModalEvents,
-             Notification) {
+import {$} from 'jquery';
+import {Ajax} from 'core/ajax';
+import {str} from 'core/str';
+import {ModalFactory} from 'core/modal_factory';
+import {ModalEvents} from 'core/modal_events';
+import {Notification} from 'core/notification';
 
-    $('.delete-btn').on('click', function() {
-        let elementId = $(this).attr('id');
-        let arr = elementId.split("-");
-        let scoreId = arr[arr.length - 1];
-        // eslint-disable-next-line promise/catch-or-return
-        ModalFactory.create({
-            type: ModalFactory.types.SAVE_CANCEL,
-            title: str.get_string('deletetitle', 'local_footballscore', '', ''),
-            body: str.get_string('modalmessage', 'local_footballscore', '', '')
-            // eslint-disable-next-line promise/always-return
-        }).then(function(modal) {
-            modal.setSaveButtonText(str.get_string('delete', 'local_footballscore', '', ''));
-            let root = modal.getRoot();
-            root.on(ModalEvents.save, function() {
-                let wsfunction = 'local_foootballscore_delete_score_by_id';
-                let params = {
-                    'scoreid': scoreId,
-                };
-                let request = {
-                    methodname: wsfunction,
-                    args: params
-                };
-                Ajax.call([request])[0].done(function() {
-                    window.location.href = $(location).attr('href');
-                }).fail(Notification.exception);
-            });
-            modal.show();
+
+
+$('.delete-btn').on('click', function () {
+    let elementId = $(this).attr('id');
+    let arr = elementId.split("-");
+    let pokemonId = arr[arr.length - 1];
+    // eslint-disable-next-line promise/catch-or-return
+    ModalFactory.create({
+        type: ModalFactory.types.SAVE_CANCEL,
+        title: str.get_string('deletetitle', 'local_pokemon_tracker', '', ''),
+        body: str.get_string('modalmessage', 'local_pokemon_tracker', '', '')
+        // eslint-disable-next-line promise/always-return
+    }).then(function (modal) {
+        modal.setSaveButtonText(str.get_string('delete', 'local_pokemon_tracker', '', ''));
+        let root = modal.getRoot();
+        root.on(ModalEvents.save, function () {
+            let wsfunction = 'local_pokemon_tracker_delete_pokemon_by_id';
+            let params = {
+                'pokemonid': pokemonId,
+            };
+            let request = {
+                methodname: wsfunction,
+                args: params
+            };
+            Ajax.call([request])[0].done(function () {
+                window.location.href = $(location).attr('href');
+            }).fail(Notification.exception);
         });
+        modal.show();
     });
 });
