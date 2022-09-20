36ddb61e1152b0a3718f86d2860cc99144f275a0
Riasat Mahbub <Riasat.Mahbub@brainstation-23.com>
2022-09-20 15:05:27 +0600

    minor changes in code to increase readibility

--- amd/src/confirmdelete.js
+++ amd/src/confirmdelete.js
@@ -14,7 +14,7 @@
 // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
 /**
- *  @module local_pokemon_tracker/confirmdelete
+ * @module local_pokemon_tracker/confirmdelete
  * @copyright 2020, Riasat Mahbub <riasat.mahbub@brainstation-23.com>
  * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
@@ -31,15 +31,17 @@ export const init = () => {
         let elementId = $(this).attr('id');
         let arr = elementId.split("_");
         let pokemonId = arr[arr.length - 1];
-        // eslint-disable-next-line promise/catch-or-return
+
         ModalFactory.create({
             type: ModalFactory.types.SAVE_CANCEL,
             title: "Confirm delete",
             body: "Do you really want to delete this amazing pokemon?"
-            // eslint-disable-next-line promise/always-return
+        
         }).then(function (modal) {
+
             modal.setSaveButtonText("Delete");
             let root = modal.getRoot();
+
             root.on(ModalEvents.save, function () {
                 let wsfunction = 'local_pokemon_tracker_delete_pokemon_by_id';
                 let params = {
@@ -49,6 +51,7 @@ export const init = () => {
                     methodname: wsfunction,
                     args: params
                 };
+  
                 Ajax.call([request])[0].done(function () {
                     window.location.href = $(location).attr('href');
                 }).fail(Notification.exception);
